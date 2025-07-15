94e5d53c12792f3deac98eef441619d6fa058003
Muhammad <mf9555759@gmail.com>
2025-07-15 19:50:30 +0800

    ui 1

--- src/App.js
+++ src/App.js
@@ -1,22 +1,24 @@
-import logo from './logo.svg';
+
 import './App.css';
 
-function App() {
+const App = () => {
+  const search = (q) => {
+    console.log({q})
+  }
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>UCUP MOVIE MANIA</h1>
+          <input type="text" placeholder='Cari film kesayangan...' className='Movie-search' onChange={({target})=> search(target.value)}/>
+        <div className="Movie-container">
+          <div className="Movie-wrapper">
+            <div className="Movie-title">Contoh Pertama</div>
+            <img className="Movie-image" src=''></img>
+            <div className="Movie-date">11-12-2022</div>
+            <div className="Movie-rate">9,9</div>
+          </div>
+        </div>
+      
       </header>
     </div>
   );
